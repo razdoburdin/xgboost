ed7e69c7ee650bf2fe84f132349f989674bdaf67
Dmitry Razdoburdin <>
2025-12-22 09:19:16 -0800

    impromve perf

--- plugin/sycl/tree/hist_dispatcher.h
+++ plugin/sycl/tree/hist_dispatcher.h
@@ -142,7 +142,7 @@ template<typename FPType>
 size_t GetRequiredBufferSize(const DeviceProperties& device_prop, size_t max_n_rows, size_t nbins,
                              size_t ncolumns, size_t max_num_bins, size_t min_num_bins) {
   using GradientPairT = xgboost::detail::GradientPairInternal<FPType>;
-  size_t max_nblocks = static_cast<size_t>((0.8 * device_prop.l2_size) / (sizeof(GradientPairT) * nbins));
+  size_t max_nblocks = device_prop.l2_size / (sizeof(GradientPairT) * nbins);
   max_nblocks = std::min(max_nblocks, HistDispatcher<FPType>::kMaxGPUUtilisation * device_prop.max_compute_units);
   // Buffer size doesn't depend on isDense flag.
   auto build_params = HistDispatcher<FPType>
